fdd9f9c5f661a6b415f99f75876f8993c068de99
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2023-03-29 22:39:42 +0600

    navbar solve

--- src/assets/Component/Header/Header.jsx
+++ src/assets/Component/Header/Header.jsx
@@ -3,23 +3,22 @@ import logo from '../../../images/Logo.svg';
 import './Header.css'
 
 const Header = () => {
-    return (
-        <div>
-        <div className="navbar header">
-  <div className="flex-1">
-    <img src={logo} alt="" />
-  </div>
-  <div className="flex-none">
-    <ul className="menu menu-horizontal px-1">
-    <li><a href='/shop'>Order</a></li>
-      <li><a href='/order'>Order Review</a></li>
-      <li><a href='/inventory'>Manage Inventory</a></li>
-      <li><a href='/login'>Login</a></li>
-    </ul>
-  </div>
-</div>
+  return (
+    <div>
+      <div className="navbar header px-12">
+      <img src={logo} alt="" />
+        
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1">
+            <li><a href='/shop'>Order</a></li>
+            <li><a href='/order'>Order Review</a></li>
+            <li><a href='/inventory'>Manage Inventory</a></li>
+            <li><a href='/login'>Login</a></li>
+          </ul>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Header;
